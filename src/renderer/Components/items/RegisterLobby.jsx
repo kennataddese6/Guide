5c00f910d9fbecfa5ee19c57e60096a44b184a1a
kennataddese6 <kennataddese6@gmail.com>
2023-08-14 11:26:39 +0300

    create register lobby componenet with its style

---
+++ src/renderer/Components/items/RegisterLobby.jsx
@@ -0,0 +1,12 @@
+import '../styles/RegisterLobby.css';
+const RegisterLobby = () => {
+  return (
+    <>
+      <div className="LobbyContainer">
+        <h1>Register Lobby</h1>
+      </div>
+    </>
+  );
+};
+
+export default RegisterLobby;
--- src/renderer/Components/pages/Register.jsx
+++ src/renderer/Components/pages/Register.jsx
@@ -1,12 +1,13 @@
 import RegisterCustomer from '../items/RegisterCustomer';
 import SideBar from '../items/SideBar';
+import RegisterLobby from '../items/RegisterLobby';
 const Register = () => {
   return (
     <>
       <div className="dashboard">
         <div className="div">
           <SideBar index={3} />
-          <RegisterCustomer role="Receptionist" />
+          <RegisterLobby />
         </div>
       </div>
     </>
