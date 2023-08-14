3921c77439f2e1395af8707cfb89f4643e84c7fd
kennataddese6 <kennataddese6@gmail.com>
2023-08-14 15:18:06 +0300

    bind all user inputs to state and a submit

--- src/renderer/Components/items/RegisterLobby.jsx
+++ src/renderer/Components/items/RegisterLobby.jsx
@@ -1,26 +1,71 @@
 import '../styles/RegisterLobby.css';
+import { useState } from 'react';
 const RegisterLobby = () => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [floorNumber, setFloorNumber] = useState('');
+
+  const handleSubmit = () => {
+    console.log('first name:', firstName);
+    console.log('first name:', lastName);
+    console.log('first name:', email);
+    console.log('first name:', phoneNumber);
+    console.log('first name:', floorNumber);
+  };
+
   return (
     <>
       <div className="LobbyContainer">
         <div className="frame">
           <div className="div">
             <div className="register-employee">Register Employee</div>
-            <input className="firstNameInput" type="text" />
+            <input
+              className="firstNameInput"
+              type="text"
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
+            />
             <div className="overlap-group">
-              <input className="lastNameInput" type="text" />
+              <input
+                className="lastNameInput"
+                type="text"
+                onChange={(e) => {
+                  setLastName(e.target.value);
+                }}
+              />
             </div>
-            <input className="emailInput" type="text" />
+            <input
+              className="emailInput"
+              type="text"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
 
-            <input className="phoneInput" type="text" />
+            <input
+              className="phoneInput"
+              type="text"
+              onChange={(e) => {
+                setPhoneNumber(e.target.value);
+              }}
+            />
 
             <div className="text-wrapper">Email</div>
             <div className="text-wrapper-4">First Name</div>
             <div className="text-wrapper-3">Last Name</div>
             <div className="text-wrapper-2">Phone Number</div>
             <div className="text-wrapper-5">Floor Number</div>
-            <input className="floorNumberInput" type="number" />
-            <div className="submitButton">
+            <input
+              className="floorNumberInput"
+              type="number"
+              onChange={(e) => {
+                setFloorNumber(e.target.value);
+              }}
+            />
+            <div className="submitButton" onClick={handleSubmit}>
               <div className="text-wrapper-6">Submit</div>
             </div>
           </div>
