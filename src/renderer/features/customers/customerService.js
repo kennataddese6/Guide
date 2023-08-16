821bc218c73d8bd82ac287c91bf75b62a9d10752
kennataddese6 <kennataddese6@gmail.com>
2023-08-16 18:44:52 +0300

    add a function that makes http request to get all cusomer

--- src/renderer/features/customers/customerService.js
+++ src/renderer/features/customers/customerService.js
@@ -15,6 +15,17 @@ const RegisterCustomer = async (userData) => {
     throw error;
   }
 };
+
+const getCustomers = async () => {
+  try {
+    const response = await axios.get(API_URL);
+    if (response.data) {
+      return response.data;
+    }
+  } catch (error) {
+    throw error;
+  }
+};
 const CustomerService = {
   RegisterCustomer,
 };
