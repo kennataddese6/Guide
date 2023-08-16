2800be0fa161a61196411cf8f38cf743f970820e
kennataddese6 <kennataddese6@gmail.com>
2023-08-16 10:10:17 +0300

    add a spinner with nice style

--- src/renderer/Components/pages/Messages.jsx
+++ src/renderer/Components/pages/Messages.jsx
@@ -1,10 +1,11 @@
 import SideBar from '../items/SideBar';
-
+import Spinner from '../Utilities/Spinner';
 const Messages = () => {
   return (
     <div className="dashboard">
       <div className="div">
         <SideBar index={2} />
+        <Spinner />
       </div>
     </div>
   );
