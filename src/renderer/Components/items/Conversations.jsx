045efbb6f269cb2b555acb1f7a11839cde120eef
kennataddese6 <kennataddese6@gmail.com>
2023-08-18 16:32:27 +0300

    show customers based on their floor in conversations

--- src/renderer/Components/items/Conversations.jsx
+++ src/renderer/Components/items/Conversations.jsx
@@ -1,28 +1,52 @@
 import '../styles/Conversations.css';
 import { getFloorCustomers } from 'renderer/features/customers/customerSlice';
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 const Conversations = ({ floorNumber }) => {
   const FloorNumber = floorNumber;
-  console.log('this is the floor number', FloorNumber);
+  const [FloorCustomers, setFloorCustomers] = useState([]);
+  const { isSuccess, message, isErrorGetCusomers, isLoadingGetCustomers } =
+    useSelector((state) => state.customer);
+
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(getFloorCustomers(FloorNumber));
   }, [FloorNumber]);
+
+  useEffect(() => {
+    if (message && message.length && !isErrorGetCusomers) {
+      console.log('this is the message that is setted', message);
+      setFloorCustomers(message);
+    }
+  }, [message]);
   return (
     <>
-      <div className="conversationCard">
-        <h3 className="customerName"> Kenna Taddese {FloorNumber}</h3>
-        <p className="customerContent">
-          Mr Kenna Taddese wants to come to Finance for Credit calculation.
-          Shall I send him ?
-        </p>
-        <p className="rcustomerContent"> Yes. Let him come</p>
-        <p className="customerContent"> I have sent Kenna Taddese</p>
-        <p className="rcustomerContent"> He has arrived</p>
-        <p className="customerContent"> Remarks:</p>
-        <p className="rcustomerTime"> 07/19/2023</p>
-      </div>
+      {FloorCustomers ? (
+        FloorCustomers.map((FloorCustomer) => (
+          <div className="conversationCard">
+            <h3 className="customerName">
+              {' '}
+              {FloorCustomer.FirstName + ' '} {FloorCustomer.LastName}
+            </h3>
+            <p className="customerContent">
+              Mr {FloorCustomer.FirstName + ' '} {FloorCustomer.LastName + ' '}
+              wants to come to {FloorCustomer.Department}. Shall I send him?
+            </p>
+            <p className="rcustomerContent"> Yes. Let him come</p>
+            <p className="customerContent">
+              {' '}
+              I have sent {FloorCustomer.FirstName + ' '}{' '}
+              {FloorCustomer.LastName}
+            </p>
+            <p className="rcustomerContent"> He has arrived</p>
+            <p className="customerContent"> Remarks:</p>
+            <p className="rcustomerTime"> 07/19/2023</p>
+          </div>
+        ))
+      ) : (
+        <></>
+      )}
     </>
   );
 };
