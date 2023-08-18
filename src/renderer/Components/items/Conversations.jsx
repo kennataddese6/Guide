68e7ae25609a34d101c2b62e8dbec50e57ee07bd
kennataddese6 <kennataddese6@gmail.com>
2023-08-18 14:24:07 +0300

    create a conversation dashboard and conversation instance

--- src/renderer/Components/items/Conversations.jsx
+++ src/renderer/Components/items/Conversations.jsx
@@ -1,10 +1,20 @@
+import '../styles/Conversations.css';
 const Conversations = ({ floorNumber }) => {
   console.log('this is the floor number', floorNumber);
   const FloorNumber = floorNumber;
   return (
     <>
-      <div>
-        <h1>Hello {FloorNumber}</h1>
+      <div className="conversationCard">
+        <h3 className="customerName"> Kenna Taddese {FloorNumber}</h3>
+        <p className="customerContent">
+          Mr Kenna Taddese wants to come to Finance for Credit calculation.
+          Shall I send him ?
+        </p>
+        <p className="rcustomerContent"> Yes. Let him come</p>
+        <p className="customerContent"> I have sent Kenna Taddese</p>
+        <p className="rcustomerContent"> He has arrived</p>
+        <p className="customerContent"> Remarks:</p>
+        <p className="rcustomerTime"> 07/19/2023</p>
       </div>
     </>
   );
