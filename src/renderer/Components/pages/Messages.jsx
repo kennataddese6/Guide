5ddded1002685ce28a59106d3f965f50477e8f9e
kennataddese6 <kennataddese6@gmail.com>
2023-08-18 10:29:46 +0300

    add floor receptionists to lobby receptionist message page

--- src/renderer/Components/pages/Messages.jsx
+++ src/renderer/Components/pages/Messages.jsx
@@ -1,13 +1,16 @@
 import SideBar from '../items/SideBar';
+import '../styles/Messages.css';
+import FloorReceptionists from '../items/FloorReceptionists';
 import Spinner from '../Utilities/Spinner';
 const Messages = () => {
   return (
-    <div className="dashboard">
-      <div className="div">
-        <SideBar index={2} />
-        <div style={{ position: 'absolute', top: '50%' }}>
-          <Spinner />
+    <div className="MessageDashboard">
+      <SideBar index={2} />
+      <div className="UserContainer">
+        <div className="userHeader">
+          <h3>Conversations</h3>
         </div>
+        <FloorReceptionists />
       </div>
     </div>
   );
