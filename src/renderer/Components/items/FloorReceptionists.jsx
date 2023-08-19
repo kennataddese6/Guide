2d28f3cc3807ed361e713d7a912e9ce14bbb05e3
kennataddese6 <kennataddese6@gmail.com>
2023-08-19 10:09:13 +0300

    diplay floor receptionists

--- src/renderer/Components/items/FloorReceptionists.jsx
+++ src/renderer/Components/items/FloorReceptionists.jsx
@@ -1,64 +1,54 @@
 import '../styles/FloorRecetionists.css';
 import Conversations from './Conversations';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getFloorReceptionists } from 'renderer/features/auth/authSlice';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 const FloorReceptionists = ({ selectedFloor, setSelectedFloor }) => {
   const dispatch = useDispatch();
+  const [floorReceptionists, setFloorReceptionists] = useState([]);
+  const { isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
   const passFloorNumber = (number) => {
     setSelectedFloor(number);
   };
   useEffect(() => {
     dispatch(getFloorReceptionists());
   }, []);
+  useEffect(() => {
+    if (isSuccess) {
+      console.log('this is the message to be setted', message);
+      setFloorReceptionists(message);
+    }
+  }, [isSuccess]);
   return (
     <>
-      <div
-        className="ReceptionistContainer"
-        style={{ backgroundColor: selectedFloor === 1 ? 'lightblue' : 'white' }}
-        onClick={() => {
-          passFloorNumber(1);
-        }}
-      >
-        <h3 className="ReceptionistName">1st Floor Receptionist</h3>
-        <p className="messageContent"> I have sent Mr.Tewodros to Sdc 13th..</p>
-        <p className="TimeandDate"> 7/19/2013</p>
-      </div>
-      <div
-        className="ReceptionistContainer"
-        style={{ backgroundColor: selectedFloor === 2 ? 'lightblue' : 'white' }}
-        onClick={() => {
-          passFloorNumber(2);
-        }}
-      >
-        <h3 className="ReceptionistName">2nd Floor Receptionist</h3>
-        <p className="messageContent"> Let Mr.Melaku come to the office..</p>
-        <p className="TimeandDate"> 6/19/2013</p>
-      </div>
-      <div
-        className="ReceptionistContainer"
-        style={{ backgroundColor: selectedFloor === 3 ? 'lightblue' : 'white' }}
-        onClick={() => {
-          passFloorNumber(3);
-        }}
-      >
-        <h3 className="ReceptionistName">3rd Floor Receptionist</h3>
-        <p className="messageContent">Mrs. Aster wants to come to the 32th..</p>
-        <p className="TimeandDate"> 5/19/2013</p>
-      </div>
-      <div
-        className="ReceptionistContainer"
-        style={{ backgroundColor: selectedFloor === 4 ? 'lightblue' : 'white' }}
-        onClick={() => {
-          passFloorNumber(4);
-        }}
-      >
-        <h3 className="ReceptionistName">4th Floor Receptionist</h3>
-        <p className="messageContent">
-          Mr. Giram has arrived at the 42th Flo...
-        </p>
-        <p className="TimeandDate"> 3/19/2013</p>
-      </div>
+      {floorReceptionists ? (
+        floorReceptionists.map((floorReceptionist) => (
+          <div
+            className="ReceptionistContainer"
+            style={{
+              backgroundColor: selectedFloor === 1 ? 'lightblue' : 'white',
+            }}
+            onClick={() => {
+              passFloorNumber(floorReceptionist.FloorNumber);
+            }}
+          >
+            <h3 className="ReceptionistName">
+              {floorReceptionist.FirstName + ' '}
+              {floorReceptionist.LastName} ({floorReceptionist.FloorNumber})
+              Floor
+            </h3>
+            <p className="messageContent">
+              {' '}
+              I have sent Mr.Tewodros to Sdc 13th..
+            </p>
+            <p className="TimeandDate"> 7/19/2013</p>
+          </div>
+        ))
+      ) : (
+        <></>
+      )}
     </>
   );
 };
