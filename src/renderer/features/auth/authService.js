de532d03d58c4c613cd0e63b505de9b1a0c20a35
kennataddese6 <kennataddese6@gmail.com>
2023-08-22 14:28:36 +0300

    set item to session storage instead of local storage

--- src/renderer/features/auth/authService.js
+++ src/renderer/features/auth/authService.js
@@ -21,7 +21,7 @@ const login = async (userData) => {
     const response = await axios.post(API_URL + 'login', userData);
 
     if (response.data) {
-      localStorage.setItem('user', JSON.stringify(response.data));
+      sessionStorage.setItem('user', JSON.stringify(response.data));
     }
     return response.data;
   } catch (error) {
