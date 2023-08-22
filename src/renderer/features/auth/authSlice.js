f85e68a61a0c0f76d9e4f99d996a13bb40cba4df
kennataddese6 <kennataddese6@gmail.com>
2023-08-22 10:23:42 +0300

    create a slice for login

--- src/renderer/features/auth/authSlice.js
+++ src/renderer/features/auth/authSlice.js
@@ -27,6 +27,23 @@ export const register = createAsyncThunk(
     }
   }
 );
+// To login user
+export const login = createAsyncThunk(
+  'auth/login',
+  async (user, thunkAPI) => {
+    try {
+      return await authService.login(user);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
 // Get Floor Receptionist
 export const getFloorReceptionists = createAsyncThunk(
   'auth/getFloorRecpetionist',
