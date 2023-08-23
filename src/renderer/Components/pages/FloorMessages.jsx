b8bd7018a4b362c269183350944e47624ab51b39
kennataddese6 <kennataddese6@gmail.com>
2023-08-23 10:36:52 +0300

    clear from message page when user logs out

--- src/renderer/Components/pages/FloorMessages.jsx
+++ src/renderer/Components/pages/FloorMessages.jsx
@@ -1,5 +1,17 @@
+import { logout } from 'renderer/features/auth/authSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import FloorSideBar from '../items/FloorSidebar';
 const FloorMessages = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { user } = useSelector((state) => state.auth);
+  useEffect(() => {
+    if (!user) {
+      navigate('/');
+    }
+  }, [user]);
   return (
     <>
       <FloorSideBar index={2} />{' '}
