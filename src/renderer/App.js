0b83a3be19aa5895cb827af7447a17a2173755a4
kennataddese6 <kennataddese6@gmail.com>
2023-08-23 09:09:00 +0300

    create a route to floor dashboard

--- src/renderer/App.js
+++ src/renderer/App.js
@@ -6,6 +6,7 @@ import './Components/styles/SideBar.css';
 import RegisterCustomer from './Components/items/RegisterCustomer';
 import Register from './Components/pages/Register';
 import Login from './Components/pages/Login';
+import FloorDashboard from './Components/pages/FloorDashboard';
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/LobbyDasboard" element={<LobbyDashboard />} />
         <Route path="/Messages" element={<Messages />} />
         <Route path="/Register" element={<Register />} />
+        <Route path="/FloorDashboard" element={<FloorDashboard />} />
       </Routes>
     </Router>
   );
