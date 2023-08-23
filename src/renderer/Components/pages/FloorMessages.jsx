1aca4b8e56a4901909ab9dc279e76bd7ef6ca02b
kennataddese6 <kennataddese6@gmail.com>
2023-08-23 11:08:38 +0300

    add a lobby receptionist contact to the floor recpetionist

--- src/renderer/Components/pages/FloorMessages.jsx
+++ src/renderer/Components/pages/FloorMessages.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import FloorSideBar from '../items/FloorSidebar';
+import FloorReceptionists from '../items/FloorReceptionists';
 const FloorMessages = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -14,9 +15,25 @@ const FloorMessages = () => {
   }, [user]);
   return (
     <>
-      <FloorSideBar index={2} />{' '}
-      <div>
-        <h1> Hello this is Floor Messages</h1>
+      <div className="MessageDashboard">
+        <FloorSideBar index={2} />{' '}
+        <div className="userHeader">
+          <h3>Conversations</h3>
+        </div>
+        <div className="UserContainer">
+          <div
+            className="ReceptionistContainer"
+            style={{ backgroundColor: 'lightblue' }}
+          >
+            <h3 className="ReceptionistName">Lobby Receptionist</h3>
+
+            <p className="messageContent">This is the message content</p>
+            <div className="img-9">L</div>
+
+            <p className="TimeandDate"> 7/19/2013</p>
+          </div>
+        </div>
+        <div></div>
       </div>
     </>
   );
