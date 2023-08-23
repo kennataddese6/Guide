313f9f432e9f086fb61419f364fbbd61a5bd272d
kennataddese6 <kennataddese6@gmail.com>
2023-08-23 09:40:54 +0300

    create a logout method from floor dashboard

--- src/renderer/Components/pages/FloorDashboard.jsx
+++ src/renderer/Components/pages/FloorDashboard.jsx
@@ -1,6 +1,25 @@
+import { logout } from 'renderer/features/auth/authSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 const FloorDashboard = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { user } = useSelector((state) => state.auth);
+  const tologout = () => {
+    dispatch(logout());
+  };
+  useEffect(() => {
+    if (!user) {
+      navigate('/');
+    }
+  }, [user]);
   return (
-    <div>
+    <div
+      onClick={() => {
+        tologout();
+      }}
+    >
       <h1> This is Floor Receptionist Dashobard</h1>
     </div>
   );
