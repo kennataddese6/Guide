91331ae8347a3a4c77c5c3656d3c0058100fe094
kennataddese6 <kennataddese6@gmail.com>
2023-08-29 17:33:13 +0300

    add icon to application

--- src/main/main.ts
+++ src/main/main.ts
@@ -91,7 +91,7 @@ const createWindow = async () => {
     show: false,
     width: 1024,
     height: 728,
-    icon: getAssetPath('icon.png'),
+    icon: path.join(__dirname, '../../assets/cbIcon.png'),
     webPreferences: {
       preload: app.isPackaged
         ? path.join(__dirname, 'preload.js')
