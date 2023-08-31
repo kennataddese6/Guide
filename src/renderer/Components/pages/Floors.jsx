3614c3445c9060d52ffe7d50a844d35b9c4e1064
kennataddese6 <kennataddese6@gmail.com>
2023-08-31 18:09:05 +0300

    create a page for displaying floors

---
+++ src/renderer/Components/pages/Floors.jsx
@@ -0,0 +1,10 @@
+import SideBar from '../items/SideBar';
+const Floors = () => {
+  return (
+    <>
+      <SideBar index={3} />
+      <h1>This is Floors page</h1>
+    </>
+  );
+};
+export default Floors;
