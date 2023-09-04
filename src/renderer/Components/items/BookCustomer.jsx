f66bf563a0879634a2d9e079cb62efa4ca81a2de
kennataddese6 <kennataddese6@gmail.com>
2023-09-04 14:58:58 +0300

    prepare a customer booking form

--- src/renderer/Components/items/BookCustomer.jsx
+++ src/renderer/Components/items/BookCustomer.jsx
@@ -1,8 +1,159 @@
+import '../styles/RegisterLobby.css';
+import { register } from '../../features/auth/authSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { MdCheckCircle } from 'react-icons/md';
+import { reset } from '../../features/auth/authSlice';
+import { MdCancel, MdError } from 'react-icons/md';
+import Spinner from '../Utilities/Spinner';
 const BookCustomer = () => {
-return (
-  <>
-    <h1> Book A customer</h1>
-  </>
-)
-}
+  const dispatch = useDispatch();
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [woreda, setWoreda] = useState('');
+  const [subCity, setSubCity] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [floorNumber, setFloorNumber] = useState('');
+  const [SuccessMessage, setSuccessMessage] = useState(false);
+  const [ErrorMessage, setErrorMessage] = useState(false);
+  const { isLoading, isError, isSuccess } = useSelector((state) => state.auth);
+  const resetInputs = () => {
+    setFirstName('');
+    setLastName('');
+    setWoreda('');
+    setPhoneNumber('');
+    setFloorNumber('');
+    setErrorMessage(false);
+    setSuccessMessage(false);
+  };
+  useEffect(() => {
+    if (isSuccess) {
+      setErrorMessage(false);
+      setSuccessMessage(true);
+    }
+    if (isError) {
+      setSuccessMessage(false);
+      setErrorMessage(true);
+    }
+    dispatch(reset());
+  }, [isSuccess, isError]);
+  const handleSubmit = () => {
+    const userData = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      phoneNumber: phoneNumber,
+      floorNumber: floorNumber,
+    };
+    console.log('This is the user data', userData);
+    resetInputs();
+  };
+
+  return (
+    <>
+      <div className="LobbyContainer">
+        <div className="frame">
+          <div className="div">
+            <div className="register-employee">Book a Customer</div>
+            {isLoading && <Spinner />}
+
+            <input
+              className="firstNameInput"
+              type="text"
+              value={firstName}
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
+            />
+            <div className="overlap-group">
+              <input
+                className="lastNameInput"
+                type="text"
+                value={lastName}
+                onChange={(e) => {
+                  setLastName(e.target.value);
+                }}
+              />
+            </div>
+            <input
+              className="woredaInput"
+              type="text"
+              value={woreda}
+              onChange={(e) => {
+                setWoreda(e.target.value);
+              }}
+            />
+            <input
+              className="subcityInput"
+              type="text"
+              value={subCity}
+              onChange={(e) => {
+                setSubCity(e.target.value);
+              }}
+            />
+            <input
+              className="phoneInput"
+              type="number"
+              value={phoneNumber}
+              onChange={(e) => {
+                setPhoneNumber(e.target.value);
+              }}
+            />
+
+            <div className="text-wrapper">Woreda</div>
+            <div className="text-wrapper-7">Subcity</div>
+            <div className="text-wrapper-4">First Name</div>
+            <div className="text-wrapper-3">Last Name</div>
+            <div className="text-wrapper-2">Phone Number</div>
+            <div className="text-wrapper-5">Floor Number</div>
+            <input
+              className="floorNumberInput"
+              type="number"
+              value={floorNumber}
+              onChange={(e) => {
+                setFloorNumber(e.target.value);
+              }}
+            />
+            <div className="submitButton">
+              <div className="text-wrapper-6">Submit</div>
+            </div>
+            {SuccessMessage ? (
+              <div
+                style={{
+                  position: 'absolute',
+                  top: '85%',
+                  color: 'green',
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
+                <MdCheckCircle color="green" />
+                <h4> Success</h4>
+              </div>
+            ) : (
+              ''
+            )}
+
+            {ErrorMessage ? (
+              <div
+                style={{
+                  position: 'absolute',
+                  top: '85%',
+                  color: 'red',
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
+                <MdCancel color="red" />
+                <h4> Error. Something went wrong</h4>
+              </div>
+            ) : (
+              ''
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 export default BookCustomer;
