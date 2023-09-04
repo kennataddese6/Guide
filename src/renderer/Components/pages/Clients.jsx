7d9d16e19898f935a762783c81dfdc629649b051
kennataddese6 <kennataddese6@gmail.com>
2023-09-04 16:58:34 +0300

    dipatch a method to get all custoemrs

--- src/renderer/Components/pages/Clients.jsx
+++ src/renderer/Components/pages/Clients.jsx
@@ -1,5 +1,17 @@
 import SideBar from '../items/SideBar';
+import { useDispatch, useSelector } from 'react-redux';
+import { getCustomers } from 'renderer/features/customers/customerSlice';
+import { useEffect, useState } from 'react';
 const Clients = () => {
+  const dispatch = useDispatch();
+  const { message } = useSelector((state) => state.customer);
+  const [allClients, setAllClients] = useState([]);
+  useEffect(() => {
+    dispatch(getCustomers());
+  }, []);
+  useEffect(() => {
+    setAllClients(message);
+  }, [message]);
   return (
     <>
       {' '}
