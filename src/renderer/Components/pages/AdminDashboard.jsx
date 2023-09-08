6370291876a1d201dd530e42052c0078df121a81
kennataddese6 <kennataddese6@gmail.com>
2023-09-08 18:05:39 +0300

    add register Floors to Admin Dashboard

--- src/renderer/Components/pages/AdminDashboard.jsx
+++ src/renderer/Components/pages/AdminDashboard.jsx
@@ -1,8 +1,10 @@
 import AdminSidebar from '../items/AdminSidebar';
+import RegisterFloors from '../items/RegisterFloors';
 const AdminDashboard = () => {
   return (
     <>
       <AdminSidebar index={1} />
+      <RegisterFloors />
     </>
   );
 };
