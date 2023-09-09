6bfdebb2948276a14c6e28e3adc9eb818ef5627a
kennataddese6 <kennataddese6@gmail.com>
2023-09-09 16:44:45 +0300

    create a floor slice and  and add register floor slice

---
+++ src/renderer/features/Floors/floorSlice.js
@@ -0,0 +1,59 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import FloorService from './floorService';
+
+// create state intial
+
+const initialState = {
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: '',
+};
+// Register Floors
+export const registerFloor = createAsyncThunk(
+  'floor/registerFloor',
+  async (floor, thunkAPI) => {
+    try {
+      return await FloorService.RegisterFloor(floor);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+export const FloorSlice = createSlice({
+  name: 'floor',
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.isLoading = false;
+      state.isSuccess = false;
+      state.isError = false;
+      state.message = '';
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      // to register Floor
+      .addCase(registerCustomer.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(registerCustomer.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.message = action.payload;
+      })
+      .addCase(registerCustomer.rejected, (state) => {
+        state.isLoading = false;
+        state.isError = true;
+      });
+  },
+});
+export const { reset } = FloorSlice.actions;
+export default FloorSlice.reducer;
