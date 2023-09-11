5baef0146c3da7b6cfeb83154ea14e8e45ba5851
kennataddese6 <kennataddese6@gmail.com>
2023-09-11 13:06:41 +0300

    get floors and set to state

--- src/renderer/Components/pages/Floors.jsx
+++ src/renderer/Components/pages/Floors.jsx
@@ -1,14 +1,25 @@
 import SideBar from '../items/SideBar';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { getFloors } from 'renderer/features/Floors/floorSlice';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getFloors, reset } from 'renderer/features/Floors/floorSlice';
 const Floors = () => {
   const dispatch = useDispatch();
-
+  const [Floors, setFloors] = useState('');
+  const { isSuccess, isError, message, isLoading } = useSelector(
+    (state) => state.floor
+  );
   useEffect(() => {
     dispatch(getFloors());
   }, []);
 
+  useEffect(() => {
+    if (isSuccess) {
+      setFloors(message);
+    }
+    if (isError) {
+    }
+    dispatch(reset());
+  }, [isSuccess, isError]);
   return (
     <>
       <SideBar index={3} />
