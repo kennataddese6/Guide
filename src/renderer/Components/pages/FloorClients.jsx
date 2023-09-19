d9a679febae7cd71596b100c092f692d8f197ca0
kennataddese6 <kennataddese6@gmail.com>
2023-09-19 18:12:04 +0300

    get floor customers at load time

--- src/renderer/Components/pages/FloorClients.jsx
+++ src/renderer/Components/pages/FloorClients.jsx
@@ -1,5 +1,20 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import FloorSideBar from '../items/FloorSidebar';
+import { getFloorCustomers } from 'renderer/features/customers/customerSlice';
 const FloorClients = () => {
+  const dispatch = useDispatch();
+  const { message } = useSelector((state) => state.customer);
+  const { user } = useSelector((state) => state.auth);
+
+  const [floorCustomers, setFloorCustomers] = useState('');
+  useEffect(() => {
+    dispatch(getFloorCustomers(user ? user.FloorNumber : ''));
+  }, []);
+  useEffect(() => {
+    setFloorCustomers(message);
+    console.log(floorCustomers);
+  }, [message]);
   return (
     <>
       <FloorSideBar index={4} />
