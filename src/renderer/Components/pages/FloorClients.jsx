f5d6558bdef274daefe24c08946a17273742ff9f
kennataddese6 <kennataddese6@gmail.com>
2023-09-19 17:52:38 +0300

    create a page to display floor clients

---
+++ src/renderer/Components/pages/FloorClients.jsx
@@ -0,0 +1,10 @@
+import FloorSideBar from '../items/FloorSidebar';
+const FloorClients = () => {
+  return (
+    <>
+      <FloorSideBar index={4} />
+      <h1> This is Floor Clients Page</h1>
+    </>
+  );
+};
+export default FloorClients;
