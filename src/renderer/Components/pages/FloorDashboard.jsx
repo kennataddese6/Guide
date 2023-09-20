e32ba05524e08e9c8f06aadf0c9d4703aeecd56e
kennataddese6 <kennataddese6@gmail.com>
2023-09-20 14:27:33 +0300

    display profile circle and client information

--- src/renderer/Components/pages/FloorDashboard.jsx
+++ src/renderer/Components/pages/FloorDashboard.jsx
@@ -1,11 +1,16 @@
-import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import FloorSideBar from '../items/FloorSidebar';
+import { getFloorCustomers } from 'renderer/features/customers/customerSlice';
 import '../styles/floorDashboard.css';
+
 const FloorDashboard = ({ online }) => {
   const navigate = useNavigate();
+  const dipatch = useDispatch();
+  const [clients, setClients] = useState([]);
   const { user } = useSelector((state) => state.auth);
+  const { message } = useSelector((state) => state.customer);
 
   useEffect(() => {
     if (!user) {
@@ -21,6 +26,12 @@ const FloorDashboard = ({ online }) => {
     }
   };
   useEffect(() => {
+    dipatch(getFloorCustomers(user ? user.FloorNumber : ''));
+  }, []);
+  useEffect(() => {
+    setClients(message);
+  }, [message]);
+  useEffect(() => {
     window.electron.ipcRenderer.on(
       'notification-clicked',
       handleNotificationClick
@@ -34,7 +45,31 @@ const FloorDashboard = ({ online }) => {
         <div className="frameHeader">
           <h3 className="headerText"> Waiting Clients</h3>
         </div>
-        <div className="clientsList"></div>
+        {clients.map((client) => (
+          <div className="clientsList">
+            <div
+              className="clientCircle"
+              alt="Avatar woman"
+              style={{
+                backgroundColor: 'grey',
+                color: 'black',
+              }}
+            >
+              {' '}
+              {client.FirstName[0].toUpperCase()}
+            </div>
+            <div className="clientInfoContainer">
+              <p>
+                {' '}
+                {client.FirstName} {client.LastName}
+              </p>
+              <p style={{ marginTop: '-10px', fontStyle: 'italic' }}>
+                {' '}
+                {client.Department}
+              </p>
+            </div>
+          </div>
+        ))}
       </div>
       <div className="secondFrame">
         <div className="frameHeader">
