e06261f71023879ecc97f8053abeefd382c7f824
kennataddese6 <kennataddese6@gmail.com>
2023-09-21 11:04:03 +0300

    clear from settings page when user logs out

--- src/renderer/Components/pages/Settings.jsx
+++ src/renderer/Components/pages/Settings.jsx
@@ -1,10 +1,20 @@
+import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import SideBar from '../items/SideBar';
 import FloorSideBar from '../items/FloorSidebar';
 import ChangePassowrd from '../items/ChangePassword';
+
 const Settings = ({ online }) => {
   const location = useLocation();
-  console.log(location.state);
+  const navigate = useNavigate();
+  const { user } = useSelector((state) => state.auth);
+  useEffect(() => {
+    if (!user) {
+      navigate('/');
+    }
+  }, [user]);
   return (
     <>
       {location.state ? (
