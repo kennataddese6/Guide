e47fae90e8f8e95ba0b9655e83a70aaffd3f20d5
kennataddese6 <kennataddese6@gmail.com>
2023-09-21 10:52:21 +0300

    render the sidebars based on location of navigation

--- src/renderer/Components/pages/Settings.jsx
+++ src/renderer/Components/pages/Settings.jsx
@@ -1,9 +1,17 @@
+import { useLocation } from 'react-router-dom';
 import SideBar from '../items/SideBar';
+import FloorSideBar from '../items/FloorSidebar';
 import ChangePassowrd from '../items/ChangePassword';
 const Settings = ({ online }) => {
+  const location = useLocation();
+  console.log(location.state);
   return (
     <>
-      <SideBar index={5} online={online} />
+      {location.state ? (
+        <SideBar index={5} online={online} />
+      ) : (
+        <FloorSideBar index={5} online={online} />
+      )}
       <ChangePassowrd />
     </>
   );
