83024881b2dd0080197738490467f0aa0b54acda
kennataddese6 <kennataddese6@gmail.com>
2023-09-22 11:42:07 +0300

    prepare inputs for dipatch

--- src/renderer/Components/items/ChangePassword.jsx
+++ src/renderer/Components/items/ChangePassword.jsx
@@ -1,5 +1,17 @@
 import '../styles/changePassword.css';
+import { useState } from 'react';
 const ChangePassowrd = () => {
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, sestConfirmPassword] = useState('');
+  const handleSubmit = () => {
+    console.log(
+      'Here are the inputs',
+      currentPassword,
+      confirmPassword,
+      newPassword
+    );
+  };
   return (
     <div className="changePassowrdContainer">
       <h3 className="headerText">Change Password</h3>
@@ -8,20 +20,35 @@ const ChangePassowrd = () => {
         className="EmailID"
         style={{ marginTop: '6em' }}
         placeholder="Current Password"
+        value={currentPassword}
+        onChange={(e) => {
+          setCurrentPassword(e.target.value);
+        }}
       />
       <input
         type="password"
         className="EmailID"
         style={{ marginTop: '2em' }}
         placeholder="New Password"
+        value={newPassword}
+        onChange={(e) => {
+          setNewPassword(e.target.value);
+        }}
       />
       <input
         type="password"
         className="EmailID"
         style={{ marginTop: '2em' }}
         placeholder="Confirm Password"
+        value={confirmPassword}
+        onChange={(e) => {
+          sestConfirmPassword(e.target.value);
+        }}
       />
-      <button className="CHangePaaswordButton"> Submit</button>
+      <button className="CHangePaaswordButton" onClick={handleSubmit}>
+        {' '}
+        Submit
+      </button>
     </div>
   );
 };
