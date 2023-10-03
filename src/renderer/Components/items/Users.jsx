750810ff71adde1e26fa863cb3cab8fa451f4169
kennataddese6 <kennataddese6@gmail.com>
2023-10-03 15:45:40 +0300

    dispatch to getusers

--- src/renderer/Components/items/Users.jsx
+++ src/renderer/Components/items/Users.jsx
@@ -1,4 +1,12 @@
+import { useDispatch } from 'react-redux';
+import { getUsers } from 'renderer/features/auth/authSlice';
+import { useEffect } from 'react';
 const Users = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getUsers());
+  }, []);
+
   return (
     <div>
       <p>This are the Users</p>
