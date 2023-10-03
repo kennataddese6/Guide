2725e4c20c20a626e48047c97905e8cea8aabe77
kennataddese6 <kennataddese6@gmail.com>
2023-10-03 13:45:42 +0300

    create a view for admin

---
+++ src/renderer/Components/pages/AdminView.jsx
@@ -0,0 +1,10 @@
+import AdminSideBar from '../items/AdminSidebar';
+const AdminView = () => {
+  return (
+    <>
+      <AdminSideBar index={4} />
+      <p>Hello this is admin page</p>
+    </>
+  );
+};
+export default AdminView;
