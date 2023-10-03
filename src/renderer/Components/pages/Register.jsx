bb947b7fc7c112ead2f606aa93f8ed7274bad5fe
kennataddese6 <kennataddese6@gmail.com>
2023-10-03 13:19:11 +0300

    add a header for regiset in admin

--- src/renderer/Components/pages/Register.jsx
+++ src/renderer/Components/pages/Register.jsx
@@ -1,9 +1,11 @@
-import RegisterCustomer from '../items/RegisterCustomer';
 import AdminSideBar from '../items/AdminSidebar';
 import RegisterLobby from '../items/RegisterLobby';
+import '../styles/AdminNavbar.css';
 const Register = () => {
   return (
     <>
+      <div className="headerOne"> Employee</div>
+      <div className="headerTwo">Floors</div>
       <AdminSideBar index={3} />
       <RegisterLobby />
     </>
