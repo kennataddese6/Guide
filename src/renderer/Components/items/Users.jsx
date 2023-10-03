507879b1b02ad844d01810fc55abc0830f0f45ae
kennataddese6 <kennataddese6@gmail.com>
2023-10-03 15:58:19 +0300

    get users and prepare them for diplay

--- src/renderer/Components/items/Users.jsx
+++ src/renderer/Components/items/Users.jsx
@@ -1,12 +1,24 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getUsers } from 'renderer/features/auth/authSlice';
 import { useEffect } from 'react';
+import { reset } from 'renderer/features/auth/authSlice';
 const Users = () => {
   const dispatch = useDispatch();
+  const { isSuccessgetFloorReceptionists, isError, message } = useSelector(
+    (state) => state.auth
+  );
   useEffect(() => {
     dispatch(getUsers());
   }, []);
-
+  useEffect(() => {
+    if (isSuccessgetFloorReceptionists) {
+      console.log('There are some users', message);
+    }
+    if (isError) {
+      console.log('sorry something went wrong');
+    }
+    dispatch(reset());
+  }, [isSuccessgetFloorReceptionists, isError]);
   return (
     <div>
       <p>This are the Users</p>
