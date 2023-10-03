c20b404fe3b0ebc30661c0da683c2126ecab3b05
kennataddese6 <kennataddese6@gmail.com>
2023-10-03 13:25:04 +0300

    add icon to header title

--- src/renderer/Components/pages/Register.jsx
+++ src/renderer/Components/pages/Register.jsx
@@ -1,11 +1,19 @@
 import AdminSideBar from '../items/AdminSidebar';
 import RegisterLobby from '../items/RegisterLobby';
 import '../styles/AdminNavbar.css';
+import { FaUser, FaBuilding } from 'react-icons/fa';
+
 const Register = () => {
   return (
     <>
-      <div className="headerOne"> Employee</div>
-      <div className="headerTwo">Floors</div>
+      <div className="headerOne">
+        <FaUser />
+        <p className="headerTtile">Employee</p>
+      </div>
+      <div className="headerTwo">
+        <FaBuilding />
+        <p className="headerTtile"> Floors</p>
+      </div>
       <AdminSideBar index={3} />
       <RegisterLobby />
     </>
