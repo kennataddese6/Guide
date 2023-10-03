cc423756a6493ae060447e9604ff5b5b348e6b6e
kennataddese6 <kennataddese6@gmail.com>
2023-10-03 14:06:01 +0300

    add header to admin view

--- src/renderer/Components/pages/AdminView.jsx
+++ src/renderer/Components/pages/AdminView.jsx
@@ -1,8 +1,31 @@
 import AdminSideBar from '../items/AdminSidebar';
+import { FaUsers, FaBuilding } from 'react-icons/fa';
+import { useState } from 'react';
+
 const AdminView = () => {
+  const [selected, setSelected] = useState(1);
   return (
     <>
       <AdminSideBar index={4} />
+      <div
+        className={selected === 1 ? 'headerOneSelected' : 'headerOne'}
+        onClick={() => {
+          setSelected(1);
+        }}
+      >
+        <FaUsers fontSize={'20px'} />
+        <p className="headerTtile"> Users</p>
+      </div>
+      <div
+        className="headerTwo"
+        className={selected === 2 ? 'headerTwoSelected' : 'headerTwo'}
+        onClick={() => {
+          setSelected(2);
+        }}
+      >
+        <FaBuilding />
+        <p className="headerTtile"> Floors</p>
+      </div>
       <p>Hello this is admin page</p>
     </>
   );
