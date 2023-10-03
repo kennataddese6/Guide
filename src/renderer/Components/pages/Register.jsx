aba6894fffff4a22635a2028bfb47c84210d985e
kennataddese6 <kennataddese6@gmail.com>
2023-10-03 13:36:12 +0300

    render register floor and employees basedn on selection

--- src/renderer/Components/pages/Register.jsx
+++ src/renderer/Components/pages/Register.jsx
@@ -1,21 +1,34 @@
 import AdminSideBar from '../items/AdminSidebar';
 import RegisterLobby from '../items/RegisterLobby';
+import RegisterFloors from '../items/RegisterFloors';
 import '../styles/AdminNavbar.css';
 import { FaUser, FaBuilding } from 'react-icons/fa';
-
+import { useState } from 'react';
 const Register = () => {
+  const [selected, setSelected] = useState(1);
   return (
     <>
-      <div className="headerOne">
+      <div
+        className={selected === 1 ? 'headerOneSelected' : 'headerOne'}
+        onClick={() => {
+          setSelected(1);
+        }}
+      >
         <FaUser />
         <p className="headerTtile">Employee</p>
       </div>
-      <div className="headerTwo">
+      <div
+        className="headerTwo"
+        className={selected === 2 ? 'headerTwoSelected' : 'headerTwo'}
+        onClick={() => {
+          setSelected(2);
+        }}
+      >
         <FaBuilding />
         <p className="headerTtile"> Floors</p>
       </div>
       <AdminSideBar index={3} />
-      <RegisterLobby />
+      {selected === 1 ? <RegisterLobby /> : <RegisterFloors />}
     </>
   );
 };
