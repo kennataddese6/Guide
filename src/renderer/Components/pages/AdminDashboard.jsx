18839e2b40b6a485beb17094d72e69ff919616aa
kennataddese6 <kennataddese6@gmail.com>
2023-10-05 15:21:34 +0300

    show report based on user choice

--- src/renderer/Components/pages/AdminDashboard.jsx
+++ src/renderer/Components/pages/AdminDashboard.jsx
@@ -1,10 +1,35 @@
 import AdminSidebar from '../items/AdminSidebar';
 import Report from '../items/Report';
+import { useState } from 'react';
+import { FaChartBar, FaChartLine } from 'react-icons/fa';
+import LineChart from '../items/LineChart';
+
 const AdminDashboard = () => {
+  const [selected, setSelected] = useState(1);
+
   return (
     <>
+      <div
+        className={selected === 1 ? 'headerOneSelected' : 'headerOne'}
+        onClick={() => {
+          setSelected(1);
+        }}
+      >
+        <FaChartBar />
+        <p className="headerTtile">Bars</p>
+      </div>
+      <div
+        className="headerTwo"
+        className={selected === 2 ? 'headerTwoSelected' : 'headerTwo'}
+        onClick={() => {
+          setSelected(2);
+        }}
+      >
+        <FaChartLine />
+        <p className="headerTtile"> Charts</p>
+      </div>
       <AdminSidebar index={1} />
-      <Report />
+      {selected === 1 ? <Report /> : <LineChart />}
     </>
   );
 };
