940df4836d74bf068e195d00a619a0f4f8d74b55
KennaTaddese <KennaTaddese@cbe.com.et>
2023-10-10 14:52:46 +0300

    show floor receptionists message

--- src/renderer/Components/items/FloorReceptionists.jsx
+++ src/renderer/Components/items/FloorReceptionists.jsx
@@ -2,6 +2,7 @@ import moment from 'moment';
 import '../styles/FloorRecetionists.css';
 import { useEffect, useState } from 'react';
 import { getFloorReceptionists } from 'renderer/features/auth/authSlice';
+import { getCustomers } from 'renderer/features/customers/customerSlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { reset } from 'renderer/features/auth/authSlice';
 import { ws } from 'renderer/webSocket';
@@ -10,27 +11,46 @@ const FloorReceptionists = ({ selectedFloor, setSelectedFloor }) => {
   const dispatch = useDispatch();
   const [floorReceptionists, setFloorReceptionists] = useState([]);
   const [incomingMessage, setIncomingMessage] = useState(false);
+  const [client, setClient] = useState([]);
 
-  const { isSuccessgetFloorReceptionists, message } = useSelector(
-    (state) => state.auth
+  const {
+    isSuccessgetFloorReceptionists,
+    message: authMessage,
+    user,
+  } = useSelector((state) => state.auth);
+  const { isSuccess, message: customerMessage } = useSelector(
+    (state) => state.customer
   );
+
   const passFloorNumber = (number) => {
     setSelectedFloor(number);
   };
   useEffect(() => {
     dispatch(getFloorReceptionists());
+    dispatch(getCustomers());
   }, []);
   useEffect(() => {
     if (isSuccessgetFloorReceptionists) {
-      console.log('this is the message to be setted', message);
-      setFloorReceptionists(message);
+      setFloorReceptionists(authMessage);
     }
     dispatch(reset());
   }, [isSuccessgetFloorReceptionists]);
+  useEffect(() => {
+    if (isSuccess) {
+      console.log('hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii', customerMessage);
+      console.log(
+        'here is the last client',
+        customerMessage
+          .filter((customers) => customers.FloorNumber === selectedFloor)
+          .filter((customer) => customer.RegisteredBy === user.Email)[0]
+      );
+      setClient(customerMessage);
+    }
+  }, [isSuccess]);
 
-  function trimMessage(message) {
+  /*   function trimMessage(message) {
     return message.length > 32 ? message.substring(0, 32) + '...' : message;
-  }
+  } */
   function formatDate(dateString) {
     const date = moment(dateString);
     const now = moment();
@@ -74,12 +94,44 @@ const FloorReceptionists = ({ selectedFloor, setSelectedFloor }) => {
                 {floorReceptionist.LastName} ({floorReceptionist.FloorNumber})
                 Floor
               </p>
-
               <p className="messageContent">
                 {' '}
-                {floorReceptionist.LatestMessage
-                  ? trimMessage(floorReceptionist.LatestMessage)
-                  : 'Sorry. Nothing to show'}
+                {client
+                  .filter(
+                    (customer) =>
+                      customer.FloorNumber === floorReceptionist.FloorNumber
+                  )
+                  .filter((customer) => customer.RegisteredBy === user.Email)[0]
+                  ?.Booking
+                  ? `Mr ${client[0]?.FirstName} would be visiting us`
+                  : client
+                      .filter(
+                        (customer) =>
+                          customer.FloorNumber === floorReceptionist.FloorNumber
+                      )
+                      .filter(
+                        (customer) => customer.RegisteredBy === user.Email
+                      )[0]?.Sent
+                  ? `I have sent ${client[0]?.FirstName}`
+                  : client
+                      .filter(
+                        (customer) =>
+                          customer.FloorNumber === floorReceptionist.FloorNumber
+                      )
+                      .filter(
+                        (customer) => customer.RegisteredBy === user.Email
+                      )[0]?.Accepted
+                  ? `Yes, let ${client[0]?.FirstName} come`
+                  : client
+                      .filter(
+                        (customer) =>
+                          customer.FloorNumber === floorReceptionist.FloorNumber
+                      )
+                      .filter(
+                        (customer) => customer.RegisteredBy === user.Email
+                      )[0]?.Waiting
+                  ? `${client[0]?.FirstName} wants to come`
+                  : `Sorry`}{' '}
               </p>
               <div className="img-9">{floorReceptionist.FirstName[0]}</div>
 
